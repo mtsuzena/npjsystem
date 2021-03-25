fe70d0fa6d2607c0c27de4bd38355d4ddbf4a79a
Victor Mateus Suzena <mateus.suzena@alta-rt.com>
2021-03-24 21:02:51 -0300

    first commit

---
+++ npjsystem/frontend/src/views/dashboard/Dashboard.vue
@@ -0,0 +1,401 @@
+<template>
+  <v-container
+    id="dashboard"
+    fluid
+    tag="section"
+    class="grey lighten-5 mb-6"
+  >
+
+    <v-row
+      align="start"
+      justify="space-between"
+      class="grey lighten-5"
+    >
+      <!-- <v-col
+        cols="12"
+        lg="4"
+      >
+        <base-material-chart-card
+          :data="emailsSubscriptionChart.data"
+          :options="emailsSubscriptionChart.options"
+          :responsive-options="emailsSubscriptionChart.responsiveOptions"
+          color="#E91E63"
+          hover-reveal
+          type="Bar"
+        >
+          <template v-slot:reveal-actions>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ attrs, on }">
+                <v-btn
+                  v-bind="attrs"
+                  color="info"
+                  icon
+                  v-on="on"
+                >
+                  <v-icon
+                    color="info"
+                  >
+                    mdi-refresh
+                  </v-icon>
+                </v-btn>
+              </template>
+
+              <span>Refresh</span>
+            </v-tooltip>
+
+            <v-tooltip bottom>
+              <template v-slot:activator="{ attrs, on }">
+                <v-btn
+                  v-bind="attrs"
+                  light
+                  icon
+                  v-on="on"
+                >
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </template>
+
+              <span>Change Date</span>
+            </v-tooltip>
+          </template>
+
+          <h4 class="card-title font-weight-light mt-2 ml-2">
+            Website Views
+          </h4>
+
+          <p class="d-inline-flex font-weight-light ml-2 mt-1">
+            Last Campaign Performance
+          </p>
+
+          <template v-slot:actions>
+            <v-icon
+              class="mr-1"
+              small
+            >
+              mdi-clock-outline
+            </v-icon>
+            <span class="text-caption grey--text font-weight-light">updated 10 minutes ago</span>
+          </template>
+        </base-material-chart-card>
+      </v-col> -->
+      <!-- <v-col
+        cols="12"
+        lg="4"
+      >
+        <base-material-chart-card
+          :data="dailySalesChart.data"
+          :options="dailySalesChart.options"
+          color="success"
+          hover-reveal
+          type="Line"
+        >
+          <template v-slot:reveal-actions>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ attrs, on }">
+                <v-btn
+                  v-bind="attrs"
+                  color="info"
+                  icon
+                  v-on="on"
+                >
+                  <v-icon
+                    color="info"
+                  >
+                    mdi-refresh
+                  </v-icon>
+                </v-btn>
+              </template>
+
+              <span>Refresh</span>
+            </v-tooltip>
+
+            <v-tooltip bottom>
+              <template v-slot:activator="{ attrs, on }">
+                <v-btn
+                  v-bind="attrs"
+                  light
+                  icon
+                  v-on="on"
+                >
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </template>
+
+              <span>Change Date</span>
+            </v-tooltip>
+          </template>
+
+          <h4 class="card-title font-weight-light mt-2 ml-2">
+            Daily Sales
+          </h4>
+
+          <p class="d-inline-flex font-weight-light ml-2 mt-1">
+            <v-icon
+              color="green"
+              small
+            >
+              mdi-arrow-up
+            </v-icon>
+            <span class="green--text">55%</span>&nbsp;
+            increase in today's sales
+          </p>
+
+          <template v-slot:actions>
+            <v-icon
+              class="mr-1"
+              small
+            >
+              mdi-clock-outline
+            </v-icon>
+            <span class="text-caption grey--text font-weight-light">updated 4 minutes ago</span>
+          </template>
+        </base-material-chart-card>
+      </v-col> -->
+      <!-- <v-col
+        cols="12"
+        lg="4"
+      >
+        <base-material-chart-card
+          :data="dataCompletedTasksChart.data"
+          :options="dataCompletedTasksChart.options"
+          hover-reveal
+          color="info"
+          type="Line"
+        >
+          <template v-slot:reveal-actions>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ attrs, on }">
+                <v-btn
+                  v-bind="attrs"
+                  color="info"
+                  icon
+                  v-on="on"
+                >
+                  <v-icon
+                    color="info"
+                  >
+                    mdi-refresh
+                  </v-icon>
+                </v-btn>
+              </template>
+
+              <span>Refresh</span>
+            </v-tooltip>
+
+            <v-tooltip bottom>
+              <template v-slot:activator="{ attrs, on }">
+                <v-btn
+                  v-bind="attrs"
+                  light
+                  icon
+                  v-on="on"
+                >
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </template>
+
+              <span>Change Date</span>
+            </v-tooltip>
+          </template>
+
+          <h3 class="card-title font-weight-light mt-2 ml-2">
+            Completed Tasks
+          </h3>
+
+          <p class="d-inline-flex font-weight-light ml-2 mt-1">
+            Last Last Campaign Performance
+          </p>
+
+          <template v-slot:actions>
+            <v-icon
+              class="mr-1"
+              small
+            >
+              mdi-clock-outline
+            </v-icon>
+            <span class="text-caption grey--text font-weight-light">campaign sent 26 minutes ago</span>
+          </template>
+        </base-material-chart-card>
+      </v-col> -->
+      <v-col
+        cols="12"
+        sm="6"
+        lg="3"
+      >
+        <base-material-stats-card
+          color="orange"
+          icon="mdi-animation-play"
+          title="Processos em andamento"
+          value="10"
+        />
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        lg="3"
+      >
+        <base-material-stats-card
+          color="red"
+          icon="mdi-alert-circle"
+          title="Processos atrasados"
+          value="15"
+        />
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        lg="3"
+      >
+        <base-material-stats-card
+          color="success"
+          icon="mdi-check"
+          title="Processos finalizados"
+          value="184"
+        />
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        lg="3"
+      >
+        <base-material-stats-card
+          color="yellow"
+          icon="mdi-animation"
+          title="Processos não iniciados"
+          value="30"
+        />
+      </v-col>
+    </v-row>
+    <v-divider class="mb-3" />
+      <div>
+      <v-sheet
+        tile
+        height="54"
+        class="d-flex"
+      >
+        <v-btn
+          icon
+          class="ma-2"
+          @click="$refs.calendar.prev()"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-select
+          v-model="type"
+          :items="types"
+          dense
+          outlined
+          hide-details
+          class="ma-2"
+          label="type"
+        ></v-select>
+        <v-select
+          v-model="mode"
+          :items="modes"
+          dense
+          outlined
+          hide-details
+          label="event-overlap-mode"
+          class="ma-2"
+        ></v-select>
+        <v-select
+          v-model="weekday"
+          :items="weekdays"
+          dense
+          outlined
+          hide-details
+          label="weekdays"
+          class="ma-2"
+        ></v-select>
+        <v-spacer></v-spacer>
+        <v-btn
+          icon
+          class="ma-2"
+          @click="$refs.calendar.next()"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-sheet>
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          v-model="value"
+          :weekdays="weekday"
+          :type="type"
+          :events="events"
+          :event-overlap-mode="mode"
+          :event-overlap-threshold="30"
+          :event-color="getEventColor"
+          @change="getEvents"
+        ></v-calendar>
+      </v-sheet>
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'DashboardDashboard',
+
+    data () {
+      return {
+        type: 'month',
+        types: ['mês', 'semana', 'dia', '4 dias'],
+        mode: 'stack',
+        modes: ['stack', 'column'],
+        weekday: [0, 1, 2, 3, 4, 5, 6],
+        weekdays: [
+          { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
+          { text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
+          { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
+          { text: 'Mon, Wed, Fri', value: [1, 3, 5] },
+        ],
+        value: '',
+        events: [],
+        colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+        names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+      }
+    },
+
+    methods: {
+      complete (index) {
+        this.list[index] = !this.list[index]
+      },
+      getEvents ({ start, end }) {
+        const events = []
+
+        const min = new Date(`${start.date}T00:00:00`)
+        const max = new Date(`${end.date}T23:59:59`)
+        const days = (max.getTime() - min.getTime()) / 86400000
+        const eventCount = this.rnd(days, days + 20)
+
+        for (let i = 0; i < eventCount; i++) {
+          const allDay = this.rnd(0, 3) === 0
+          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+          const second = new Date(first.getTime() + secondTimestamp)
+
+          events.push({
+            name: this.names[this.rnd(0, this.names.length - 1)],
+            start: first,
+            end: second,
+            color: this.colors[this.rnd(0, this.colors.length - 1)],
+            timed: !allDay,
+          })
+        }
+
+        this.events = events
+      },
+      getEventColor (event) {
+        return event.color
+      },
+      rnd (a, b) {
+        return Math.floor((b - a + 1) * Math.random()) + a
+      },
+    },
+  }
+</script>
---
+++ npjsystem/frontend/src/views/dashboard/components/core/Footer.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-footer
+    id="dashboard-core-footer"
+    v-bind="localAttrs"
+    :padless="padless"
+  >
+    <v-container>
+      <v-row
+        align="center"
+        no-gutters
+      >
+        <v-col
+          v-for="(link, i) in links"
+          :key="i"
+          class="text-center mb-sm-0 mb-5"
+          cols="auto"
+        >
+          <a
+            :href="link.href"
+            class="mr-0 grey--text text--darken-3"
+            rel="noopener"
+            target="_blank"
+            v-text="link.text"
+          />
+        </v-col>
+
+        <v-spacer class="hidden-sm-and-down" />
+
+        <v-col
+          cols="12"
+          md="auto"
+        >
+          <div class="text-body-1 font-weight-light pt-6 pt-md-0 text-center">
+            &copy; 2021, desenvolvido por<a>NPJ System</a>.
+            <!-- <v-icon size="18">
+              mdi-heart
+            </v-icon> -->
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    name: 'DashboardCoreFooter',
+
+    data: () => ({
+      links: [
+        {
+          href: '#',
+          text: 'NPJ System',
+        },
+        {
+          href: '#',
+          text: 'Sobre Nós',
+        },
+        {
+          href: '#',
+          text: 'Licenças',
+        },
+      ],
+      padless: false,
+      variant: 'default',
+    }),
+    computed: {
+      localAttrs () {
+        const attrs = {}
+
+        if (this.variant === 'default') {
+          attrs.absolute = true
+          attrs.fixed = true
+        } else {
+          attrs[this.variant] = true
+        }
+        return attrs
+      },
+    },
+  }
+</script>
+
+<style lang="sass">
+  #dashboard-core-footer
+    a
+      font-size: .825rem
+      font-weight: 500
+      text-decoration: none
+      text-transform: uppercase
+</style>
